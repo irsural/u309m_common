57aae467a7f93592bdcb2ff0c72d8b7651d25c6b
GavrishchukMaxim <Zudwa87@gmail.com>
2012-10-23 12:26:32 +0000

    git-svn-id: https://5-10/svn/u309m_common@8 784819df-0e49-ba48-a535-3ffbe095aa23

--- common_data.h
+++ common_data.h
@@ -497,7 +497,7 @@ struct control_data_t
   irs::bit_data_t alarm_17A_th_base;
   irs::bit_data_t alarm_17A_th_aux;
   irs::bit_data_t alarm_upper_level;
-  
+
   irs::bit_data_t on;
 
   irs::conn_data_t<irs_u8> unlock;
@@ -518,9 +518,10 @@ struct control_data_t
   irs::bit_data_t watchdog_reset_cause;
   irs::bit_data_t watchdog_test;
   irs::bit_data_t izm_th_spi_enable;
-  
+
   irs::conn_data_t<irs_u32> connect_counter;
   irs::conn_data_t<irs_u32> time;
+  irs::conn_data_t<irs_u32> work_counter;
 
   control_data_t(irs::mxdata_t *ap_data = IRS_NULL, irs_uarc a_index = 0,
     irs_uarc* ap_size = IRS_NULL)
@@ -591,12 +592,13 @@ struct control_data_t
     watchdog_reset_cause.connect(ap_data, index, 4);
     watchdog_test.connect(ap_data, index, 5);
     izm_th_spi_enable.connect(ap_data, index, 6);
-    
+
     index++;
     index++;
 
     index = connect_counter.connect(ap_data, index);
     index = time.connect(ap_data, index);
+    index = work_counter.connect(ap_data, index);
 
     return index;
   }
@@ -617,7 +619,7 @@ struct eth_data_t {
   supply_eth_data_t supply_2V;    //  114 bytes
   supply_eth_data_t supply_1A;    //  114 bytes
   supply_eth_data_t supply_17A;   //  114 bytes
-  control_data_t control;         //  18 bytes
+  control_data_t control;         //  20 bytes
   //---------------------------------------------
   //                          Итого:  668 байт
 
